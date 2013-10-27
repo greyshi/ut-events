29091da177bdc558986c261bf2588714a9e6e9e5
Damien Johnson <dmn.johns@gmail.com>
2013-10-27 15:24:16 -0500

    Added some routing for RESTful API calls
    Flask documentation:
    http://flask.pocoo.org/docs/quickstart/

--- webapp.py
+++ webapp.py
@@ -1,16 +1,33 @@
 import os
 from flask import Flask
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def root():
     return 'TODO: Splash page!'
 
-@app.route('/submission/')
-def submission():
-    return 'TODO: Submission form'
-
 @app.route('/about/')
-def submission():
+def about():
     return 'TODO: About page'
+
+@app.route('/submit/', methods=['GET', 'POST', 'PUT'])
+def submit():
+    """TODO: submission form"""
+    if request.method == 'POST':
+        title = request.form('title')
+        """Add row(s) to the database"""
+    else:
+        return render_template('submit.html')
+    
+"""This is the RESTful API call to get """
+@app.route('/events/')
+@app.route('/events/<int:event_id>')
+def events(event_id=None):
+    return 'TODO: Return XML of request event(s) from database'
+    
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
