18064b9f829de76124a9eb875330ff1b10516848
Alex Yu <iu@utexas.edu>
2013-10-29 23:01:15 -0500

    listview

--- UTEvents/src/com/utevents/EventListActivity.java
+++ UTEvents/src/com/utevents/EventListActivity.java
@@ -1,35 +1,47 @@
 package com.utevents;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class EventListActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_list);
-        
-        // Initially, a single TextView should display 'Fetching events...'
-        
-        // Make an API call to our web service to get the events
-        
-        // Translate the XML response into view(s) (TextViews with background colors
-        // and specific formatting? width=fill_parent, add side padding, length=1 or
-        // whatever weight works to fix x events on a page) The parent View for the
-        // events should be scrollable (ListView).
-        
-        // Replace the initial TextView with the new TextViews created from the
-        // data fetched from the database.
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_event_list);
+
+		// Initially, a single TextView should display 'Fetching events...'
+
+		// Make an API call to our web service to get the events
+
+		// Translate the XML response into view(s) (TextViews with background colors
+		// and specific formatting? width=fill_parent, add side padding, length=1 or
+		// whatever weight works to fix x events on a page) The parent View for the
+		// events should be scrollable (ListView).
+
+		// Replace the initial TextView with the new TextViews created from the
+		// data fetched from the database.
+		final ListView listview = (ListView) findViewById(R.id.content_list);
+		
+		
+		listview.setAdapter(ArrayAdapter.createFromResource(this, R.array.test, R.layout.list_item));
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.event_list, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.event_list, menu);
+		return true;
+	}
+
 }
