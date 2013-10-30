604a0248d20355136a1579ec1ccac0e35ca18562
Damien Johnson <damien.johnson@rackspace.com>
2013-10-30 02:23:28 -0500

    Added API call, cleaned up some code, created Event class to store Event data

---
+++ UTEvents/src/com/utevents/Event.java
@@ -0,0 +1,82 @@
+package com.utevents;
+
+import java.util.Date;
+
+public class Event {
+
+	private String title;
+	private String location;
+	private String description;
+	private Date startTime;
+	private Date endTime;
+	
+	Event (String title, String location, Date startTime) {
+		this.title = title;
+		this.location = location;
+		this.startTime = startTime;
+	}
+	
+	Event (String title, String location, Date startTime, Date endTime) {
+		this.title = title;
+		this.location = location;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+	
+	Event (String title, String location, Date startTime, Date endTime, String description) {
+		this.title = title;
+		this.location = location;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.description = description;
+	}
+	
+	// TODO: Fix constructors so that an Event can be created with either an endTime,
+	//       a description, neither, or both.
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public Date getStartTime() {
+		return startTime;
+	}
+	
+	public Date getEndTime() {
+		return endTime;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	
+	public String toString() {
+		// TODO: Implement
+		return "TODO";
+	}
+	
+}
--- UTEvents/src/com/utevents/EventListActivity.java
+++ UTEvents/src/com/utevents/EventListActivity.java
@@ -1,16 +1,18 @@
 package com.utevents;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class EventListActivity extends Activity {
 
@@ -19,21 +21,39 @@ public class EventListActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_event_list);
 
+		Event[] eventsArray;
+		
+		ArrayList<Event> events = new ArrayList<Event>();
+		
+		final TextView fetchingText = (TextView) findViewById(R.id.fetching_text);
+		final ListView listView = (ListView) findViewById(R.id.content_list);
+		
 		// Initially, a single TextView should display 'Fetching events...'
 
 		// Make an API call to our web service to get the events
-
-		// Translate the XML response into view(s) (TextViews with background colors
+		try {
+			events = fetchEvents();
+		} catch (Exception e) {
+			// TODO: Error handling
+		}
+		
+		// Translate the events into view(s) (TextViews with background colors
 		// and specific formatting? width=fill_parent, add side padding, length=1 or
 		// whatever weight works to fix x events on a page) The parent View for the
 		// events should be scrollable (ListView).
-
+		
+		//listView.setAdapter(ArrayAdapter.createFromResource(this, R.array.test, R.layout.list_item));
+		eventsArray = events.toArray(new Event[events.size()]);
+		listView.setAdapter(new ArrayAdapter<Event>(this, R.layout.list_item, eventsArray));
+		
+		// NOTE: To use something other than TextViews for the array display, for instance, ImageViews, 
+		//       or to have some of data besides toString() results fill the views, override 
+		//       getView(int, View, ViewGroup) to return the type of view you want.
+		
 		// Replace the initial TextView with the new TextViews created from the
 		// data fetched from the database.
-		final ListView listview = (ListView) findViewById(R.id.content_list);
-		
-		
-		listview.setAdapter(ArrayAdapter.createFromResource(this, R.array.test, R.layout.list_item));
+		fetchingText.setVisibility(View.GONE); // Remove the starter text
+		listView.setVisibility(View.VISIBLE); // Display list view
 	}
 
 
@@ -42,6 +62,39 @@ public class EventListActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.event_list, menu);
 		return true;
+	}
+	
+	// A helper method to make a call to our web API and return a list of events.
+	private ArrayList<Event> fetchEvents() throws Exception {
+		ArrayList<Event> events;
+		BufferedReader response;
+		String responseLine;
+		StringBuilder responseString;
+		
+		URL url = new URL("http://utevents.herokuapp.com/events");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/xml");
+		
+		if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+			response = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			
+			responseString = new StringBuilder();
+			while ((responseLine = response.readLine()) != null) {
+				responseString.append(responseLine);
+			}
+			
+			response.close();
+		} else {
+			return null;
+		}
+		
+		events = new ArrayList<Event>();
+		// TODO: Unmarshall XML from responseString into Event objects and stuff
+		//       those objects into events. (JAXB)
+		
+		return events;
 	}
 
 }
